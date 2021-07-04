6ba67ed52a312209d8c854a6188b8c8121de9c21
JingAn Chen <chen.jingan@icloud.com>
2021-07-05 00:31:33 +0800

    Done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const currentDirectory = process.cwd();
+import fs from "fs";
+import path from "path";
+import trimImage from "trim-image";
+
+const CURRENT_DIRECTORY = process.cwd();
+
+const files = (await fs.promises.readdir(CURRENT_DIRECTORY)).filter((f) =>
+	[".png", ".jpeg", ".jpg", ".bmp"].includes(path.extname(f))
+);
+
+for (const f of files) {
+	trimImage(f, f);
+}
