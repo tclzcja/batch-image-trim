2f8a9a14faf3ec3f9f4fd26231f84dc4c77a4fec
JingAn Chen <chen.jingan@icloud.com>
2021-07-05 10:21:04 +0800

    Use pixel-level process to trim

--- index.js
+++ index.js
@@ -6,17 +6,35 @@ import jimp from "jimp";
 
 const CURRENT_DIRECTORY = process.cwd();
 
+console.clear();
+
 const files = (await fs.promises.readdir(CURRENT_DIRECTORY)).filter((f) =>
-	[".png", ".jpeg", ".jpg", ".bmp"].includes(path.extname(f))
+	[".png", ".jpeg", ".jpg", ".bmp", ".tiff"].includes(path.extname(f))
 );
 
 for (const f of files) {
-	console.log("Progressing", f);
+	console.log("Processing", f);
+
 	let image = await jimp.read(f);
-	image
-		.quality(100)
-		.autocrop({
-			cropOnlyFrames: true,
-		})
-		.write(f);
+
+	let edgeTop = image.bitmap.height;
+	let edgeLeft = image.bitmap.width;
+	let edgeRight = 0;
+	let edgeBottom = 0;
+
+	for (let y = 0; y < image.bitmap.height; y++) {
+		for (let x = 0; x < image.bitmap.width; x++) {
+			const rgba = jimp.intToRGBA(image.getPixelColor(x, y));
+			if (rgba.a > 0) {
+				edgeTop = Math.min(y, edgeTop);
+				edgeBottom = Math.max(y, edgeBottom);
+				edgeLeft = Math.min(x, edgeLeft);
+				edgeRight = Math.max(x, edgeRight);
+			}
+		}
+	}
+
+	image.crop(edgeLeft, edgeTop, edgeRight - edgeLeft, edgeBottom - edgeTop);
+
+	image.write(f);
 }
