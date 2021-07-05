76d52cf42cad341743528e72d233107c1a694ac6
JingAn Chen <chen.jingan@icloud.com>
2021-07-05 10:52:00 +0800

    Add some image files

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ const CURRENT_DIRECTORY = process.cwd();
 
 console.clear();
 
+const filesToRename = (await fs.promises.readdir(CURRENT_DIRECTORY)).filter(
+	(f) => [".jpeg", ".jpg"].includes(path.extname(f))
+);
+
+for (const f of filesToRename) {
+	await fs.promises.rename(
+		f,
+		f.replace(".jpg", ".png").replace(".jpeg", ".png")
+	);
+}
+
 const files = (await fs.promises.readdir(CURRENT_DIRECTORY)).filter((f) =>
 	[".png", ".jpeg", ".jpg", ".bmp", ".tiff"].includes(path.extname(f))
 );
